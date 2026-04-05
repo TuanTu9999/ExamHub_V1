d540c7e05e28f24349b908f4e6ce9fe9599ec6ef
Nguyễn Tuấn Tú <148473606+TuanTu9999@users.noreply.github.com>
2026-04-05 17:46:38 +0700

    Added missing functions: renderSavedQuestions, quickLogin and fixed syntax errors.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Initialization code...
+
+// Missing core functions added
+function renderSavedQuestions() {
+    // TODO: Implement function logic
+}
+
+function quickLogin() {
+    // TODO: Implement function logic
+}
+
+// Remaining code...
